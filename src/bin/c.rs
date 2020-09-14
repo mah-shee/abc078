43d73518bdffb2007cd9f4c90ceb78db4f00dbf5
masa <masatoshi.sboard@gmail.com>
2020-09-14 11:49:17 +0900

    Done c

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -3,5 +3,10 @@ use proconio::marker::{Bytes, Chars, Usize1};
 use proconio::{fastout, input};
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        m: usize,
+    }
+    let ans = (1900 * m + 100 * (n - m)) * 2u32.pow(m as u32) as usize;
+    println!("{}", ans);
 }
